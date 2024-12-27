9c5bb404ef3d931160a172905777454835aa9e47
mirayatech <mirayaabrodi@gmail.com>
2024-12-27 15:49:52 +0100

    feat: create main Page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import { AnimePreview } from "./components/AnimePreview";
+import { AnimeTitleText } from "./components/AnimeTitleText";
+import { useMobileWarning } from "./hooks/useMobileWarning";
+import { useMousePosition } from "./hooks/useMousePosition";
+import { ANIME_TITLES } from "./lib/constant";
+import { AnimeSceneEntry, data } from "./lib/data";
+import "./App.css";
+import { Toaster } from "sonner";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Page = () => {
+  const [hoveredText, setHoveredText] = useState<string | null>(null);
+  const mousePosition = useMousePosition();
+  useMobileWarning();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="relative flex w-screen flex-col items-center justify-center">
+      <Toaster />
+      <div className="flex flex-col items-center justify-center gap-4 text-nowrap text-5xl font-black uppercase text-zinc-300 *:cursor-default md:text-7xl">
+        {ANIME_TITLES.map((title) => (
+          <AnimeTitleText
+            key={title.id}
+            title={title}
+            onHover={setHoveredText}
+            onHoverEnd={() => setHoveredText(null)}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <AnimatePresence>
+        {hoveredText &&
+          data[hoveredText].map((item: AnimeSceneEntry, index: number) => (
+            <AnimePreview
+              key={index}
+              hoveredText={hoveredText}
+              item={item}
+              index={index}
+              mousePosition={mousePosition}
+            />
+          ))}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Page;
